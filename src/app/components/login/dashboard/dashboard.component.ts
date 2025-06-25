00b85befa7dbe4617b227354fc09c6fdda72dcbd
Bhupati Gudla <bhupatigudla00@gmail.com>
2025-06-25 12:38:30 +0530

    added paypal button

--- src/app/components/login/dashboard/dashboard.component.ts
+++ src/app/components/login/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, inject, PLATFORM_ID } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -6,8 +6,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatTimepickerModule} from '@angular/material/timepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { isPlatformBrowser } from '@angular/common';
 
 
 @Component({
@@ -28,7 +29,11 @@ import {provideNativeDateAdapter} from '@angular/material/core';
 })
 export class DashboardComponent {
 
+  paymentRef = viewChild('paymentRef', { read: ElementRef });
+
   travelForm: FormGroup;
+
+  private platformId = inject(PLATFORM_ID);
 
   constructor(private fb: FormBuilder) {
     this.travelForm = this.fb.group({
@@ -40,6 +45,65 @@ export class DashboardComponent {
       year: [''],
       time: ['']
     });
+  }
+
+
+  // ngOnInit(): void {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     console.log('PayPal object:', (window as any).paypal);
+  //     const paypal = (window as any).paypal;
+  //     paypal.Buttons().render(this.paymentRef()?.nativeElement);
+  //   //   paypal.Buttons(
+  //   //     {
+  //   //         style: {
+  //   //           layout: 'horizontal',
+  //   //           color: 'blue',
+  //   //           shape: 'rect',
+  //   //           label: 'paypal'
+  //   //         }
+  //   //     }
+  //   //   ).render(this.paymentRef()!.nativeElement);
+  //   // }
+  // }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const paypal = (window as any).paypal;
+      // paypal.Buttons().render(this.paymentRef()?.nativeElement);
+      paypal.Buttons(
+        {
+          style: {
+            layout: 'horizontal',
+            color: 'blue',
+            shape: 'rect',
+            label: 'paypal'
+          },
+          createOrder: (data: any, actions: any) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: "5",
+                    currency_code: 'USD'
+                  }
+                }
+              ]
+            })
+          },
+          onApprove: (data: any, actions: any) => {
+            return actions.order.capture().then((details: any) => {
+              alert("Payment is successful...")
+              console.log("Below is the order details.");
+              console.log(details);
+            })
+          },
+          onError: (error: any) => {
+            console.log(error);
+            alert("Payment is failed...")
+          }
+        }
+      ).render(this.paymentRef()!.nativeElement);
+    }
   }
 
   onSubmit() {
